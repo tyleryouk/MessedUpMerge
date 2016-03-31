d1cd91f82dbea475f2bf2a9c02131a4737c4e8f7
Tyler Youk <tyleryouk@Tylers-iMac.home>
2016-03-30 20:54:43 -0400

    Merging

--- SpaceUp/Views/InterstitialAdView.swift
+++ SpaceUp/Views/InterstitialAdView.swift
@@ -24,17 +24,6 @@ class InterstitialAdView: UIView {
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
-  // MARK: - Presentation
-    /*override func viewDidLoad() {
-        super.viewDidLoad()
-        interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
-        
-        var request = GADRequest()
-        // Requests test ads on test devices.
-        request.testDevices = ["2077ef9a63d2b398840261c8221a0c9b"]
-        interstitial!.loadRequest(request)
-    }*/
     
   func presentInterstitialAd(interstitialAd: ADInterstitialAd) {
     interstitialAd.presentInView(self)
